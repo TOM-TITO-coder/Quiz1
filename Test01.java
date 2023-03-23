67d1275fc13799cc85aa5f408f4b3bd1a17e4fcf
teang sreyroth <teangsreyrotha508@gmail.com>
2023-03-23 17:03:37 +0700

    Update Test01.java

--- Test01.java
+++ Test01.java
@@ -1,7 +1,7 @@
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hi");
+        System.out.println("Hi im Roth");
     }
 
 }
